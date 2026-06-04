4b18569812dc05fecc4ea43c2ea1586d0cbe832a
Manuel Peñalva <mpenalva@fi.uba.ar>
2026-06-04 00:15:11 -0300

    Agrega respuestas desde ConnectionActor a la app

--- station/src/connection.rs
+++ station/src/connection.rs
@@ -24,6 +24,14 @@ impl ConnectionActor {
             response: ctx.address(),
         });
     }
+
+    fn send_response<T>(&mut self, response: T) 
+    where 
+        T: Serializable,
+    {
+        let response_text = response.serialize();
+        let _ = self.socket.write_all(response_text.as_bytes());
+    }
 }
 
 impl Actor for ConnectionActor {
@@ -72,5 +80,37 @@ impl StreamHandler<std::io::Result<Vec<u8>>> for ConnectionActor {
                 _ctx.stop();
             }
         }
+    }
+}
+
+impl Handler<RentConfirmed> for ConnectionActor {
+    type Result = ();
+
+    fn handle(&mut self, msg: RentConfirmed, _ctx: &mut Self::Context) {
+        self.send_response(msg);
+    }
+}
+
+impl Handler<RentRejected> for ConnectionActor {
+    type Result = ();
+
+    fn handle(&mut self, msg: RentRejected, _ctx: &mut Self::Context) {
+        self.send_response(msg);
+    }
+}
+
+impl Handler<ReturnConfirmed> for ConnectionActor {
+    type Result = ();
+
+    fn handle(&mut self, msg: ReturnConfirmed, _ctx: &mut Self::Context) {
+        self.send_response(msg);
+    }
+}
+
+impl Handler<ReturnRejected> for ConnectionActor {
+    type Result = ();
+
+    fn handle(&mut self, msg: ReturnRejected, _ctx: &mut Self::Context) {
+        self.send_response(msg);
     }
 }
